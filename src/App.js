0fea7045144f7d540aa22352227708724fc2315f
IonTulbure <ion.tulbure@feel-it-services.com>
2024-04-03 12:27:51 +0300

    create App component

--- src/App.js
+++ src/App.js
@@ -9,4 +9,26 @@ function Todo({ todo }) {
   );
 };
 
+function App() {
+  const [todos, setTodos] = React.useState([
+    { text: "Learn about React" },
+    { text: "Meet friend for lunch" },
+    { text: "Build really cool todo app" }
+  ]);
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
 export default App;
