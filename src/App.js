e44f3556e31573fbf7e98f858ca390d72c4a62bd
IonTulbure <ion.tulbure@feel-it-services.com>
2024-04-03 12:35:02 +0300

    create TodoForm component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,28 @@ function Todo({ todo }) {
   );
 };
 
+function TodoForm({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
 function App() {
   const [todos, setTodos] = React.useState([
     { text: "Learn about React" },
